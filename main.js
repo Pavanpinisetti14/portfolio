9ff3e61fa5991df455669fe4d9a5d6a469de96cf
pavan <70502985+Pavanpinisetti14@users.noreply.github.com>
2024-08-03 17:39:11 +0530

    updated

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = "none";
+}
+
+// Close the modal when clicking anywhere outside of it
+window.onclick = function(event) {
+  const modals = document.getElementsByClassName('modal');
+  for (let i = 0; i < modals.length; i++) {
+      if (event.target == modals[i]) {
+          modals[i].style.display = "none";
+      }
+  }
+}
